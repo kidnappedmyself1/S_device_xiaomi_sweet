141ed499015a3a3989891550d6a64011bcf43c16
Niranjan BR <niranjankannan2003@gmail.com>
2021-10-08 18:31:00 +0530

    Legion Bringup
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_sweet.mk
+    $(LOCAL_DIR)/legion_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_sweet-eng \
-    arrow_sweet-userdebug \
-    arrow_sweet-user
+    legion_sweet-eng \
+    legion_sweet-userdebug \
+    legion_sweet-user
--- legion_sweet.mk
+++ legion_sweet.mk
@@ -5,17 +5,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common Legion OS configurations
+$(call inherit-product, vendor/legion/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_sweet
+PRODUCT_NAME := legion_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-
+TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-DEVICE_MAINTAINER := daniml3
-TARGET_INCLUDE_PIXEL_CHARGER := true
-TARGET_BOOT_ANIMATION_RES := 1080
+# Legion OS Stuffs
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.maintainer.name=mrfox2003
+LEGION_MAINTAINER := mrfox2003
+TARGET_FACE_UNLOCK_SUPPORTED := true
