70b4a4d6a5f9e5ea98ad6d7bac89431000003b6d
kidnappedmyself1 <97450347+kidnappedmyself1@users.noreply.github.com>
2022-01-16 15:16:36 +0530

    Update and rename awaken_sweet.mk to potato_sweet.mk

--- potato_sweet.mk
+++ potato_sweet.mk
@@ -6,13 +6,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit common AOSP configurations
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 USE_PIXEL_CHARGING := true
 TARGET_USES_BLUR := true
 TARGET_GAPPS_ARCH := arm64
 
-PRODUCT_NAME := awaken_sweet
+PRODUCT_NAME := potato_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
